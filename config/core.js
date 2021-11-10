9ab8fa62f55a7270415649661a7d0a7697d14109
caseymm <casey@bocoup.com>
2021-11-09 17:11:55 -0800

    try to force fix?

--- config/core.js
+++ config/core.js
@@ -4,12 +4,13 @@ const { Baker } = require('@datagraphics/baker');
 const baker = new Baker({
   assets: 'assets',
   data: '_data',
-  domain: 'https://yvonnemelisande.github.io/portfolio/_dist',
+  domain: 'https://yvonnemelisande.github.io/portfolio/portfolio/_dist',
   entrypoints: 'scripts/app.js',
   input: process.cwd(),
   layouts: '_layouts',
   output: '_dist',
-  pathPrefix: 'portfolio/' || '/',
+  // pathPrefix: 'portfolio/' || '/',
+  pathPrefix: '',
 });
 
 module.exports = { baker };
